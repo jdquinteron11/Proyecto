0b3047c05c5eaebbd2de2c48abbebd985cd7c631
Juan Diego Quintero Naranjo <juan.quintero66236@ucaldas.edu.co>
2025-12-03 14:35:51 -0500

    Fisrt commit

---
+++ src/MainApp.java
@@ -0,0 +1,7 @@
+// java
+public class Main {
+    public static void main(String[] args) {
+        // Delegar al main de la UI que está en el paquete ui
+        ui.MainApp.main(args);
+    }
+}
---
+++ src/modelo/Rol.java
@@ -0,0 +1,4 @@
+package modelo;
+
+public class Rol_Enum {
+}
