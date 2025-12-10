f8d6b64c62b67d7f1627db6a3f89d171a2c1c4ea
maria jose <maria.gonzalez65404@ucaldas.edu.co>
2025-12-10 12:17:46 -0500

    interfaces

---
+++ src/ui/newUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class newUI {
+    private JPanel inicio;
+    private JButton INICIARbt;
+    private JButton REGISTRARbt;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
